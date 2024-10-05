f8107eb150f746ac1bddb41a61adaebc1f490f9e
Derks12 <148168249+Derks12@users.noreply.github.com>
2024-10-05 05:52:31 -0500

    ola tengo sueño

--- app/src/main/java/com/example/iot_lab4_20191641/MainActivity.java
+++ app/src/main/java/com/example/iot_lab4_20191641/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.iot_lab4_20191641;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,5 +29,53 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AppActivity.class);
+            startActivity(intent);
+        });
+
+        if (!tieneInternet()) {
+            showNoInternetDialog();
+        }
     }
+
+    private boolean tieneInternet() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        boolean tieneInternet = false;
+
+        if (connectivityManager != null) {
+            Network network = connectivityManager.getActiveNetwork();
+            if (network != null) {
+                NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(network);
+                if (capabilities != null) {
+                    if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                        Log.i("msg-Internet", "NetworkCapabilities.TRANSPORT_CELLULAR");
+                        tieneInternet = true;
+                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                        Log.i("msg-Internet", "NetworkCapabilities.TRANSPORT_WIFI");
+                        tieneInternet = true;
+                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                        Log.i("msg-Internet", "NetworkCapabilities.TRANSPORT_ETHERNET");
+                        tieneInternet = true;
+                    }
+                }
+            }
+        }
+
+        return tieneInternet;
+    }
+
+    private void showNoInternetDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("No hay conexión a Internet")
+                .setPositiveButton("Configuración", (dialog, which) -> {
+                    startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                })
+                .setNegativeButton("Cancelar", null)
+                .show();
+    }
+
 }
